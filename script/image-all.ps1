b1bc86de93c4e192db7054c045ac6e020c763c7c
Daniel Fang <qkfang@hotmail.com>
2025-04-12 09:12:03 +1000

    add posts

--- script/image-all.ps1
+++ script/image-all.ps1
@@ -2,13 +2,13 @@
 # $mdxFiles = Get-ChildItem -Path . -Filter *ai-app*.mdx | Select-Object -First 10
 $mdxFiles = Get-ChildItem -Path . -Filter *.mdx
 
-# # Loop through each file
-# foreach ($file in $mdxFiles) {
-#     # Pass the filename to image-setup.ps1
-#     Write-Host "Processing file: $($file.Name)"
-#     ..\script\image-setup.ps1 $file.Name
-#     Write-Host "--------------------------"
-# }
+# Loop through each file
+foreach ($file in $mdxFiles) {
+    # Pass the filename to image-setup.ps1
+    Write-Host "Processing file: $($file.Name)"
+    ..\script\image-setup.ps1 $file.Name
+    Write-Host "--------------------------"
+}
 
 # Loop through each file
 foreach ($file in $mdxFiles) {
